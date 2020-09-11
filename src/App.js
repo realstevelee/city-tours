a44b528dcd51f1bbdb9e940d82cc70997d414274
realstevelee <realstevelee@gmail.com>
2020-09-11 00:33:22 -0400

    added TourList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar'
+import TourList from './components/TourList'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 class App extends Component {
   render() {
     return <React.Fragment>
       <Navbar />
+      <TourList />
     </React.Fragment>
   }
 }
---
+++ src/components/TourList/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import Tour from '../Tour'
+import "./tourlist.scss"
+import { tourData } from '../../tourData'
+
+export default class TourList extends Component {
+    state = {
+        tours: tourData
+    }
+
+    removdTour = id => {
+        console.log(id)
+    }
+
+    render() {
+        //console.log(this.state.tours)
+
+        const { tours } = this.state;
+
+        return (
+            <section className="tourlist">
+                {
+                    tours.map(tour => (<Tour key={tour.id} tour={tour}
+                        removeTour={this.removdTour} />))
+                }
+            </section>
+        )
+    }
+}

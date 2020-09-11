7c89fb5be2e1aaf9d14ac92292b5d14bd25aba7f
realstevelee <realstevelee@gmail.com>
2020-09-10 23:51:07 -0400

    navbar updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
+import Navbar from './components/Navbar/Navbar'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-function App() {
-  return (
-    <h1>Our current app</h1>
-  );
+class App extends Component {
+  render() {
+    return <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  }
 }
 
 export default App;
